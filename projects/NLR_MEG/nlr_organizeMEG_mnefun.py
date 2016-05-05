204af41fd4d8e2dca5923f6e698e0d99911e0739
jyeatman <jyeatman@stanford.edu>
2016-05-04 18:16:45 -0700

    Previously we were only using subjects with exactly 8 sessions. However there were some subjects with empty room recordings that were throwing this off. So now we look for >=8 sessions and we string match to exclude sessions with naming that does not match our naming scheme

--- projects/NLR_MEG/nlr_organizeMEG_mnefun.py
+++ projects/NLR_MEG/nlr_organizeMEG_mnefun.py
@@ -41,7 +41,7 @@ def nlr_organizeMEG_mnefun(raw_dir=None, out_dir=None, subs=None, sess=None):
             fiflist = glob.glob(os.path.join(raw_dir, s, ss, '*_raw.fif'))
             # Sort the list
             fiflist.sort()
-            if len(fiflist) == 8:
+            if len(fiflist) >= 8:
                 # make a directory for the raw files to be saved
                 rfifdir = os.path.join(sdir, 'raw_fif')
                 subjects.append(s+ss)  # append subjects with the subjectsession
@@ -52,18 +52,34 @@ def nlr_organizeMEG_mnefun(raw_dir=None, out_dir=None, subs=None, sess=None):
                     os.mkdir(rfifdir)
                 # Loop over the list of raw fif files
                 for fnum, fname in enumerate(fiflist):
-                    fifdest = os.path.join(rfifdir, s+ss+fname[-10:])
-                    # WE ONLY COPY THE FILE OVER IF IT DOESN'T EXIST!
-                    if not os.path.isfile(fifdest):
-                        # Copy this fif file to the destination and rename it to
-                        # have the subject name. This is the assumption in mnefun
-                        shutil.copy(os.path.join(raw_dir, s, ss, fname), fifdest)
+                    if not runnumber(fname):
+                        # Skip this file if it doesn't match the naming scheme
+                        # Defined in runnumber
+                        print('Skipping ' + fname)
                     else:
-                        print(fifdest + ' already exists: Skpping')
-                    # Make the prebad file that mnefun wants
-                    prebad = os.path.join(rfifdir, s+ss+'_prebad.txt')
-                    if not os.path.isfile(prebad):
-                        open(prebad, 'w').close()
+                        fifdest = os.path.join(rfifdir, s + ss + '_' + runnumber(fname))
+                        # WE ONLY COPY THE FILE OVER IF IT DOESN'T EXIST!
+                        if not os.path.isfile(fifdest):
+                            # Copy this fif file to the destination and rename it to
+                            # have the subject name. This is the assumption in mnefun
+                            shutil.copy(os.path.join(raw_dir, s, ss, fname), fifdest)
+                        else:
+                            print(fifdest + ' already exists: Skpping')
+                        # Make the prebad file that mnefun wants
+                        prebad = os.path.join(rfifdir, s+ss+'_prebad.txt')
+                        if not os.path.isfile(prebad):
+                            open(prebad, 'w').close()
 
     # Return a list of all the subjects
     return(subjects)
+
+def runnumber(fifname):
+    fifend = []
+    runnums =['1_raw.fif', '2_raw.fif', '3_raw.fif', '4_raw.fif',
+    '5_raw.fif', '6_raw.fif', '7_raw.fif', '8_raw.fif']
+    for rn in runnums:
+        if fifname.find(rn) > -1:
+            fifend = rn
+    return fifend            
+            
+ 
