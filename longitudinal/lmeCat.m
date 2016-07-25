4d0df4348f3d055c25d4c6a9694e29b4e512e845
jyeatman <jyeatman@.uw.edu>
2016-07-25 15:07:39 -0700

    commented out the centering

--- longitudinal/lmeCat.m
+++ longitudinal/lmeCat.m
@@ -5,19 +5,19 @@ function [lme_linear, data_table] = lmeCat(sid, long_var, score)
 
 % Center each individual's reading scores
 s = unique(sid);
-for ii = 1:length(s)
-   index = find(strcmp(s(ii),sid));
-   total = 0;
-   for jj = 1:length(index)
-       total = plus(total, score(index(jj))); 
-   end
-   avg = total/length(index);
-   
-   for kk = 1:length(index);
-       score_sq_unique(index(kk), 1) = score(index(kk)) - avg;
-   end
-end
-score_adj = score_sq_unique;
+% for ii = 1:length(s)
+%    index = find(strcmp(s(ii),sid));
+%    total = 0;
+%    for jj = 1:length(index)
+%        total = plus(total, score(index(jj))); 
+%    end
+%    avg = total/length(index);
+%    
+%    for kk = 1:length(index);
+%        score_sq_unique(index(kk), 1) = score(index(kk)) - avg;
+%    end
+% end
+% score_adj = score_sq_unique;
 
 
 long_var = categorical(long_var);
