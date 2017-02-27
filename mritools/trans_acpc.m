8ac2d7009e14f38e0e54a945f12b6b6b62465a67
sjjoo <sjjoo@utexas.edu>
2017-02-27 14:57:32 -0800

    Update average anat

--- mritools/trans_acpc.m
+++ mritools/trans_acpc.m
@@ -7,14 +7,15 @@
 %
 
 %% AC-PC align
-sub = 'NLR_132_WP'; % set the subject id
+sub = 'NLR_187_NB'; % set the subject id -- we're not using it at the moment...
 
-filelist = {'/mnt/diskArray/projects/MRI/NLR_132_WP/20160919/raw/NLR_132_WP_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
-    '/mnt/diskArray/projects/MRI/NLR_132_WP/20161010/raw/NLR_132_WP_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
-    '/mnt/diskArray/projects/MRI/NLR_132_WP/20161122/raw/NLR_132_WP_WIP_MEMP_VBM_SENSE_14_1.nii.gz'%, ...
-%     '/mnt/diskArray/projects/MRI/NLR_132_WP/20160815/raw/NLR_102_RS_WIP_MEMP_VBM_SENSE_14_1.nii.gz'
+% Change filelist per subject
+filelist = {'/mnt/diskArray/projects/MRI/NLR_187_NB/20161017/raw/NLR_187_NB_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
+    '/mnt/diskArray/projects/MRI/NLR_187_NB/20161103/raw/NLR_187_NB_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
+    '/mnt/diskArray/projects/MRI/NLR_187_NB/20161119/raw/NLR_187_NB_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
+    '/mnt/diskArray/projects/MRI/NLR_187_NB/20161205/raw/NLR_187_NB_WIP_MEMP_VBM_SENSE_14_1.nii.gz'
     };
-
+ 
 outpath = sprintf('/mnt/diskArray/projects/anatomy/%s',sub);
 
 if isempty(dir(outpath))
@@ -24,16 +25,18 @@ voxres = [0.8, 0.8, 0.8];
 
 for i = 1: length(filelist)
     if isempty(dir(filelist{i}))
-        system(sprintf('parrec2nii -o -c %s %s.par',filelist{i}(1:51),filelist{i}(1:end-7)));
+        system(sprintf('parrec2nii -c -o %s %s.par',filelist{i}(1:51),filelist{i}(1:end-7)));
     end
     temp = readFileNifti(filelist{i});
     tempmontage = makeMontage(temp.data,176:265);
     imagesc(tempmontage), colormap('gray')
+    drawnow;
+    pause;
 end
 
 % Only include good session numbers here!!!
-gind = [1,3,4];
+gind = [1,2,3];
 
 filelist = filelist(gind);
 
-outImg = mrAnatAverageAcpcNifti(filelist, sprintf('%s/t1_acpc_avg.nii.gz',outpath), [], voxres(1:3))
+outImg = mrAnatAverageAcpcNifti(filelist, sprintf('%s/t1_acpc_avg.nii.gz',outpath), [], voxres(1:3));
