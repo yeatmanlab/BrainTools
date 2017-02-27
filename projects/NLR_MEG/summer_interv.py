864957d0509c524bf1e29c77e64ff72111cb2b0d
sjjoo <sjjoo@utexas.edu>
2017-02-27 13:54:01 -0800

    More MEG analysis

--- projects/NLR_MEG/summer_interv.py
+++ projects/NLR_MEG/summer_interv.py
@@ -45,9 +45,9 @@ os.chdir(out_dir)
 # 208_lh missing
 # 210_sb missing first session 
 # 151_rd missing the second session
-subs = ['102_rs','110_hh','145_ac','150_mg','151_rd','152_tc','160_ek','161_ak','162_ef',
-    '163_lf','164_sf','170_gm','172_th','174_hs','179_gm','180_zd','207_ah','210_sb','211_lb',
-    '201_gs','202_dd','203_am','204_am']
+subs = ['102_rs','103_ac','110_hh','145_ac','150_mg','151_rd','152_tc','160_ek',
+    '161_ak','162_ef','163_lf','164_sf','170_gm','172_th','174_hs','179_gm','180_zd','207_ah','210_sb','211_lb',
+    '201_gs','202_dd','203_am','204_am','206_lm','105_bb','127_am']
 
 # tmin, tmax: sets the epoch
 # bmin, bmax: sets the prestim duration for baseline correction. baseline is set
@@ -61,12 +61,12 @@ subs = ['102_rs','110_hh','145_ac','150_mg','151_rd','152_tc','160_ek','161_ak',
 # correction by setting bmin and bmax. I found that mnefun does baseline 
 # correction by default.
 # sjjoo_20160809: Commented
-params = mnefun.Params(tmin=-0.1, tmax=1.0, t_adjust=-39e-3, n_jobs=18,
+params = mnefun.Params(tmin=-0.1, tmax=1.0, t_adjust=-39e-3, n_jobs=18, # t_adjust was -39e-3
                        decim=2, n_jobs_mkl=1, proj_sfreq=250,
                        n_jobs_fir='cuda', n_jobs_resample='cuda',
                        filter_length='5s', epochs_type='fif', lp_cut=40.,
 #                       hp_cut=0.15,hp_trans=0.1,
-                       bmin=-0.1, auto_bad=25., plot_raw=False, 
+                       bmin=-0.1, auto_bad=15., plot_raw=False, 
                        bem_type = '5120-5120-5120')
           
 # This sets the position of the head relative to the sensors. These values a
@@ -78,7 +78,7 @@ params.trans_to = (0., 0., .03)
 
 params.sss_type = 'python'
 params.sss_regularize = 'svd' # 'in' by default
-params.tsss_dur = 8. # 60 for adults with not much head movements. This was set to 6.
+params.tsss_dur = 16. # 60 for adults with not much head movements. This was set to 6.
 
 params.auto_bad_meg_thresh = 10 # THIS SHOULD NOT BE SO HIGH!
 
@@ -103,23 +103,26 @@ print(out)
 """
 
 # REMOVE BAD SUBJECTS
-badsubs = ['102_rs150716','110_hh150824','152_tc160510','152_tc160527',
-           '201_gs150818','201_gs150824','201_gs150908','162_ef160829',
+badsubs = ['127_am123_md','102_rs150716','110_hh150824','152_tc160510','152_tc160527',
+           '201_gs150818','201_gs150824','201_gs150908',
            '203_am150922','203_am151009','202_dd150827','202_dd151013',
-           '204_am151020',# These are not included
-#           '102_rs160618','102_rs160815','110_hh160608','110_hh160809',
-#           '145_ac160621','145_ac160823','150_mg160606','150_mg160825',
+           '204_am151020','206_lm160202'# These are not included
+#           '102_rs160618','102_rs160815','103_ac150609','110_hh160608','110_hh160809',
+#           '145_ac160621','145_ac160823'
+#            ,'150_mg160606','150_mg160825',
 #           '151_rd160620','152_tc160422','152_tc160623','160_ek160627','160_ek160915',
 #           '161_ak160627','161_ak160916',
-#           '163_lf160707','163_lf160920','164_sf160707','164_sf160920',
-#           '170_gm160613','170_gm160822',
+#           '162_ef160829','163_lf160707','163_lf160920','164_sf160707','164_sf160920',
+#           '170_gm160613',
+#           '170_gm160822',
 #           '172_th160614','172_th160825','174_hs160620','174_hs160829',
 #           '179_gm160701','179_gm160913','180_zd160621','180_zd160826',
 #           '207_ah160608','207_ah160809','210_sb160822','211_lb160617',
 #           '211_lb160823','201_gs150729','201_gs150925',
-#           '202_dd150919','202_dd151013','202_dd151103','203_am150831',
-#           '203_am151029','204_am150829','204_am151120'# These are already processed successfully
-           ]
+#           '202_dd150919','202_dd151103','203_am150831',
+#           '203_am151029','204_am150829','204_am151120',# These are already processed successfully
+#           '206_lm151119','206_lm160113'
+        ]
 for n, s in enumerate(badsubs):
     subnum = out.index(s)
     print('Removing subject ' + str(subnum) + ' ' + out[subnum])
@@ -127,8 +130,11 @@ for n, s in enumerate(badsubs):
     ind[subnum] = []
     ind.remove([])
 
-for n in np.arange(0,len(out)):
-    params.subjects = [out[n]]
+for n, s in enumerate(out):
+    print(s)    
+    
+for n, s in enumerate(out):
+    params.subjects = [s]
     
     #print("Running " + str(len(params.subjects)) + ' Subjects') 
 #    print("\n\n".join(params.subjects))
@@ -144,15 +150,15 @@ for n in np.arange(0,len(out)):
     #params.subject_indices = np.arange(27,len(params.subjects))
     params.structurals =[None] * len(params.subjects)
     
-    if n == 17:
+    if s == '164_sf160707':
         params.run_names = ['%s_1', '%s_2', '%s_3', '%s_5','%s_6'] # 164_sf160707
-    elif n == 19:
+    elif s == '170_gm160613':
         params.run_names = ['%s_1', '%s_2', '%s_3', '%s_5','%s_6'] # 170_gm160613
-    elif n == 22:
+    elif s == '172_th160825':
         params.run_names = ['%s_1', '%s_2', '%s_3', '%s_4', '%s_5'] # 172_th160825
-    elif n == 34:
+    elif s == '201_gs150729':
         params.run_names = ['%s_1', '%s_3', '%s_4', '%s_5', '%s_6'] # 201_gs150729
-    elif n == 41:
+    elif s == '204_am151120':
         params.run_names = ['%s_1', '%s_3', '%s_4', '%s_5', '%s_6'] # 204_am151120
     else:
         params.run_names = ['%s_1', '%s_2', '%s_3', '%s_4','%s_5','%s_6']
@@ -185,25 +191,32 @@ for n in np.arange(0,len(out)):
     #params.mf_args = '-hpie 30 -hpig .8 -hpicons' # sjjoo-20160826: We are doing SSS using python
     
     # epoch rejection criterion
-    if n == 17 or 18: # 164_sf160707 or 164_sf160920
-        params.reject = dict(grad=4000e-13, mag=6.0e-12)
+    # 207_ah: grad = 5000e-13, mag = 5.0e-12
+    if s == '163_lf160707' or s == '163_lf160920':
+        params.reject = dict(grad=7000e-13, mag=9.0e-12)
+#    elif s == '150_mg160606':
+#        params.reject = dict(grad=5000e-13, mag=5.0e-12)
+#    elif s == '170_gm160822':
+#        params.reject = dict(grad=5000e-13, mag=6.0e-12)
+    elif s == '162_ef160829':
+        params.reject = dict(grad=7000e-13, mag=7.0e-12)
+#    elif s == '207_ah160608':
+#        params.reject = dict(grad=5000e-13, mag=5.0e-12)
+#    elif s == '172_th160825':  
+#        params.reject = dict(grad=6000e-13, mag=6.0e-12)
+    elif s == '174_hs160620':
+        params.reject = dict(grad=5000e-13, mag=9.0e-12)
+    elif s == '174_hs160829':
+        params.reject = dict(grad=10000e-13, mag=12.0e-12)
     else:
-        params.reject = dict(grad=4000e-13, mag=5.0e-12)
+        params.reject = dict(grad=5000e-13, mag=5.0e-12)
         
-    params.ssp_eog_reject = dict(grad=4000e-13, mag=5.0e-12, eog=np.inf)
-    params.ssp_ecg_reject = dict(grad=4000e-13, mag=5.0e-12, ecg=np.inf)
+    params.ssp_eog_reject = dict(grad=params.reject['grad'], mag=params.reject['mag'], eog=np.inf)
+    params.ssp_ecg_reject = dict(grad=params.reject['grad'], mag=params.reject['mag'], ecg=np.inf)
         
     params.flat = dict(grad=1e-13, mag=1e-15)
     
-    if n == 24: # 174_hs160829
-        params.auto_bad_reject = dict(grad=10000e-13, mag=13.0e-12) # This should be high...
-        params.tsss_dur = 8.
-    elif n == 16: # 163_lf160920
-        params.auto_bad_reject = dict(grad=9000e-13, mag=11.0e-12)
-        params.tsss_dur = 8.
-    else:
-        params.auto_bad_reject = dict(grad=9000e-13, mag=10.0e-12) 
-        params.tsss_dur = 8.        
+    params.auto_bad_reject = dict(grad=2*params.reject['grad'], mag=2*params.reject['mag'])         
         
     params.auto_bad_flat = params.flat
       
@@ -214,8 +227,8 @@ for n in np.arange(0,len(out)):
     params.inv_runs = [range(0, len(params.run_names))]
     params.runs_empty = []
     
-    params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
-                        [1, 1, 0],  # EOG # sjjoo-20160826: was 3
+    params.proj_nums = [[3, 3, 0],  # ECG: grad/mag/eeg
+                        [3, 3, 0],  # EOG # sjjoo-20160826: was 3
                         [0, 0, 0]]  # Continuous (from ERM)
     
     # The scoring function needs to produce an event file with these values
@@ -264,23 +277,23 @@ for n in np.arange(0,len(out)):
         # Before running SSS, make SUBJ/raw_fif/SUBJ_prebad.txt file with
         # space-separated list of bad MEG channel numbers
         push_raw=False,      # Push raw files and SSS script to SSS workstation
-        do_sss=False,        # Run SSS remotely (on sws) or locally with mne-python
+        do_sss=True,        # Run SSS remotely (on sws) or locally with mne-python
         fetch_sss=False,     # Fetch SSSed files from SSS workstation
         do_ch_fix=False,     # Fix channel ordering
     
         # Before running SSP, examine SSS'ed files and make
         # SUBJ/bads/bad_ch_SUBJ_post-sss.txt; usually, this should only contain EEG
         # channels.
-        gen_ssp=False,       # Generate SSP vectors
-        apply_ssp=False,     # Apply SSP vectors and filtering
+        gen_ssp=True,       # Generate SSP vectors
+        apply_ssp=True,     # Apply SSP vectors and filtering
         plot_psd=False,      # Plot raw data power spectra
-        write_epochs=False,  # Write epochs to disk
+        write_epochs=True,  # Write epochs to disk
         gen_covs=True,      # Generate covariances
     
         # Make SUBJ/trans/SUBJ-trans.fif using mne_analyze; needed for fwd calc.
         gen_fwd=False,       # Generate forward solutions (and src space if needed)
         gen_inv=False,       # Generate inverses
-        gen_report=False,    # Write mne report html of results to disk
+        gen_report=True,    # Write mne report html of results to disk
         print_status=False,  # Print completeness status update
     
 #        params,
