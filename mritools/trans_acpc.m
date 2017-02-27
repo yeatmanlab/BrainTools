04425090e56c4bed22d4bdc8c04111e624b40296
sjjoo <sjjoo@utexas.edu>
2017-02-27 13:52:11 -0800

    Add acpc align and average

--- mritools/trans_acpc.m
+++ mritools/trans_acpc.m
@@ -7,26 +7,37 @@
 %
 
 %% AC-PC align
+sub = 'NLR_132_WP'; % set the subject id
+
 filelist = {'/mnt/diskArray/projects/MRI/NLR_102_RS/20160618/raw/NLR_102_RS_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
-    '/mnt/diskArray/projects/MRI/NLR_102_RS/20160708/raw/NLR_102_RS_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
-    '/mnt/diskArray/projects/MRI/NLR_102_RS/20160726/raw/NLR_102_RS_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
-    '/mnt/diskArray/projects/MRI/NLR_102_RS/20160815/raw/NLR_102_RS_WIP_MEMP_VBM_SENSE_14_1.nii.gz'};
-outpath = '/mnt/diskArray/projects/anatomy/test/';
+    '/mnt/diskArray/projects/MRI/NLR_132_WP/20161010/raw/NLR_132_WP_WIP_MEMP_VBM_SENSE_14_1.nii.gz', ...
+    '/mnt/diskArray/projects/MRI/NLR_132_WP/20161122/raw/NLR_132_WP_WIP_MEMP_VBM_SENSE_14_1.nii.gz'%, ...
+%     '/mnt/diskArray/projects/MRI/NLR_132_WP/20160815/raw/NLR_102_RS_WIP_MEMP_VBM_SENSE_14_1.nii.gz'
+    };
+
+outpath = sprintf('/mnt/diskArray/projects/anatomy/%s',sub);
+
 if isempty(dir(outpath))
     mkdir(outpath);
 end
 voxres = [0.8, 0.8, 0.8];
 
-system(sprintf('freeview -v %s',filelist{1}));
+for i = 1: length(filelist)
+    %     system(sprintf('fslview %s',filelist{i}));
+    temp = readFileNifti(filelist{i});
+    tempmontage = makeMontage(temp.data,176:265);
+    imagesc(tempmontage), colormap('gray')
+end
 
-system(sprintf('freeview -v %s',filelist{2}));
+% system(sprintf('freeview -v %s',filelist{2}));
+% 
+% system(sprintf('freeview -v %s',filelist{3}));
+% 
+% system(sprintf('freeview -v %s',filelist{4}));
 
-system(sprintf('freeview -v %s',filelist{3}));
-
-system(sprintf('freeview -v %s',filelist{4}));
-
+% Only include good session numbers here!!!
 gind = [1,3,4];
 
-filelist = filelist(gind)
+filelist = filelist(gind);
 
 outImg = mrAnatAverageAcpcNifti(filelist, sprintf('%s/t1_acpc_avg.nii.gz',outpath), [], voxres(1:3))
