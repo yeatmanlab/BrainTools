c655e5217630a120b06e5f5b61ca5898c5117581
Mark Penrod <mark.penrod@gmail.com>
2017-07-11 12:24:35 -0700

    script for base template

---
+++ mpenrod/fs_base_recon.m
@@ -0,0 +1,19 @@
+% function which constructs and runs the recon-all -base command based on a
+% given subject ID
+function fs_base_recon(ID)
+    freesurf_dir = '/mnt/scratch/projects/freesurfer/';
+    addpath(genpath(freesurf_dir));
+    for ii = 1:5
+        if ~exist(fullfile(freesurf_dir, strcat(ID,'_', num2str(ii))), 'file')
+            break
+        end
+    end
+    num_sess = ii - 1;
+    recon_cmd = strcat('recon-all -base', strcat([' ', ID], '_template'));
+    for ii = 1:num_sess
+        recon_cmd = strcat(recon_cmd, [' ','-tp'],...
+            strcat([' ', ID], '_', num2str(ii)));
+    end
+    recon_cmd = [recon_cmd, ' -all'];
+    system(recon_cmd)
+end
