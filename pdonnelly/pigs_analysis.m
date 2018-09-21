2d4bd1a7c1763a95fff1e81357e6ba7433a7f706
Patrick Donnelly <pdonnelly92@gmail.com>
2018-09-21 14:34:46 -0700

    added mixed effect analyses for practice interactions

--- pdonnelly/pigs_analysis.m
+++ pdonnelly/pigs_analysis.m
@@ -5,12 +5,16 @@
 
 %% Read in Data
 % Run pigs_analysis script in Python notebook
-data = readtable('~/Desktop/pigs_wordlist_data.csv');
+
+data = readtable('C://Users/Patrick/Desktop/pigs_wordlist_data.csv');
 categorical(data.int_session);
 categorical(data.pigs_casecontrol);
+data.worddiff = data.pigs_word2_acc - data.pigs_word1_acc;
+data.pseudodiff = data.pigs_pseudo2_acc - data.pigs_pseudo1_acc;
+
 
 data2 = stack(data,{'pigs_word1_acc','pigs_word2_acc','pigs_pseudo1_acc','pigs_pseudo2_acc'},'NewDataVariableName','acc')
-data2.type = data2.acc_Indicator=='pigs_word1_acc' | data2.acc_Indicator=='pigs_word2_acc'
+data2.type = data2.acc_Indicator=='pigs_word1_acc' | data2.acc_Indicator=='pigs_word2_acc';
 %% LME Model
 word_simple_model = 'word_acc ~ 1 + int_session + pigs_casecontrol + (1|record_id)'
 word_model = 'word_acc ~ 1 + pigs_casecontrol*int_session + (1|record_id)'
@@ -22,15 +26,21 @@ pseudo_lme = fitlme(data, pseudo_model, 'FitMethod', 'ML')
 %% 
 
 % Real words
-lme1 = fitlme(data2(data2.type==true,:), 'acc ~ 1 + pigs_casecontrol*int_session + (1|record_id)', 'FitMethod', 'ML')
-lme2 = fitlme(data2(data2.type==true,:), 'acc ~ 1 + pigs_casecontrol*int_session + (1|record_id) + (1|acc_Indicator)', 'FitMethod', 'ML')
+lme1 = fitlme(data2(data2.type==true,:), 'acc ~ 1 + wj_brs + pigs_practice_numstories + pigs_casecontrol*int_session + (1|record_id)', 'FitMethod', 'ML')
+lme2 = fitlme(data2(data2.type==true,:), 'acc ~ 1 + pigs_practice_numstories * pigs_casecontrol*int_session + (1|record_id) + (1|acc_Indicator)', 'FitMethod', 'ML')
 compare(lme1,lme2)
 lme3 = fitlme(data2(data2.type==true,:), 'acc ~ 1 + pigs_casecontrol*int_session + (int_session|record_id) + (1|acc_Indicator)', 'FitMethod', 'ML')
 compare(lme2,lme3)
 
 % Pseudo
-lme1 = fitlme(data2(data2.type==false,:), 'acc ~ 1 + pigs_casecontrol*int_session + (1|record_id)', 'FitMethod', 'ML')
-lme2 = fitlme(data2(data2.type==false,:), 'acc ~ 1 + pigs_casecontrol*int_session + (1|record_id) + (1|acc_Indicator)', 'FitMethod', 'ML')
+lme1 = fitlme(data2(data2.type==false,:), 'acc ~ 1 + pigs_practice_numstories*pigs_casecontrol*int_session + (1|record_id)', 'FitMethod', 'ML')
+
+data2.int_session = categorical(data2.int_session);
+data2.pigs_casecontrol = categorical(data2.pigs_casecontrol);
+data2.wj_brs = zscore(data2.wj_brs);
+data2.pigs_practice_numstories = zscore(data2.pigs_practice_numstories);
+lme2 = fitlme(data2(data2.type==false,:), 'acc ~ 1 + pigs_practice_numstories*pigs_casecontrol*int_session + (1|record_id) + (1|acc_Indicator)', 'FitMethod', 'ML'...
+)
 compare(lme1,lme2)
 lme3 = fitlme(data2(data2.type==false,:), 'acc ~ 1 + pigs_casecontrol*int_session + (int_session|record_id)', 'FitMethod', 'ML')
 compare(lme1,lme3)
