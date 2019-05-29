3f3d50ad85799931ca91e922947e77b2e64bf37c
jyeatman <jyeatman@.uw.edu>
2019-05-29 11:26:33 -0700

    added more options

--- mritools/fs_vol2surf.m
+++ mritools/fs_vol2surf.m
@@ -1,7 +1,22 @@
-function fs_vol2surf(movingIm, subjid, hemi, outname)
+function fs_vol2surf(movingIm, subjid, hemi, outname, projdist)
 % Map data in a nifti image to a subjects cortical surface
+% 
+% fs_vol2surf(movingIm, subjid, hemi, outname, [projdist])
+%
+% Use defaults unless projecdist is defined
+%
+% example:
+% fs_vol2surf('/mnt/scratch/freesurfer/PreK_EK/roi3.nii.gz','PreK_EK','rh',...
+% '/mnt/scratch/freesurfer/PreK_EK/roi3.mgz', [-1 4 1])
 
-            % Map to Freesurfer surface
-            cmd = sprintf('mri_vol2surf --mov %s --regheader %s --o %s --hemi %s --surf white --projfrac 1',...
-                movingIm, subjid, outname, hemi);
-            system(cmd)
+
+% Map to Freesurfer surface
+if ~exist('projdis','var') || isempty(projdist)
+    
+    cmd = sprintf('mri_vol2surf --mov %s --regheader %s --o %s --hemi %s --surf white',...
+        movingIm, subjid, outname, hemi);
+else
+    cmd = sprintf('mri_vol2surf --mov %s --regheader %s --o %s --hemi %s --surf white --projdist-avg %d %d %d',...
+        movingIm, subjid, outname, hemi, projdist);
+end
+system(cmd)
